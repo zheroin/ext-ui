ead8d9247b65210ef11eb9bac2483982cbbcfeb6
CleanCodeCreature <lambertlee410@gmail.com>
2023-04-12 15:17:41 -0400

    Check filelist

--- src/components/iframe.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const RandomTaskIframe: React.FC = () => (
-  <iframe src="https://www.amplemarket.com/login" className="w-full h-full" />
-)
-
-export default RandomTaskIframe
